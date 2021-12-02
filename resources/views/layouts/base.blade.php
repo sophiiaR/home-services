316aa210e05ecaf80bc682801a10142b106da521
Sofia <sraissiguier@gmail.com>
2021-12-02 11:27:02 -0300

    Add conditional rendering to layout base view to access users' dasboards

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -119,8 +119,38 @@
                             <li><a href="servicesbycategory/18.html">Home Automation</a></li>
                         </ul>
                     </li>
-                    <li class="login-form"> <a href="index.php/register.html" title="Register">Register</a></li>
-                    <li class="login-form"> <a href="login.html" title="Login">Login</a></li>
+                    @if (Route::has('login'))
+                        @auth
+                            @if (Auth::user()->utype === 'ADM')
+                                <li class="login-form"> <a href="#" title="Register">My Account (Admin)</a>
+                                    <ul class="drop-down one-column hover-fade">
+                                        <li><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+                                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+                                    </ul>
+                                </li>
+                            @elseif(Auth::user()->utype === 'SVP')
+                                <li class="login-form"> <a href="#" title="Register">My Account (Service Provider)</a>
+                                    <ul class="drop-down one-column hover-fade">
+                                        <li><a href="{{route('sprovider.dashboard')}}">Dashboard</a></li>
+                                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+                                    </ul>
+                                </li>
+                            @else
+                                <li class="login-form"> <a href="#" title="Register">My Account (Customer)</a>
+                                    <ul class="drop-down one-column hover-fade">
+                                        <li><a href="{{route('customer.dashboard')}}">Dashboard</a></li>
+                                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+                                    </ul>
+                                </li>
+                            @endif
+                            <form id="logout-form" method="POST" action="{{route('logout')}}" style="display: none">
+                                @csrf
+                            </form>
+                        @else
+                            <li class="login-form"> <a href="{{route('register')}}" title="Register">Register</a></li>
+                            <li class="login-form"> <a href="{{route('login')}}" title="Login">Login</a></li>
+                        @endif
+                    @endif
                     <li class="search-bar">
                     </li>
                 </ul>

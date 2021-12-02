2fa9fe639742c45e28d78777aebd0575908952b2
Sofia <sraissiguier@gmail.com>
2021-12-02 12:02:58 -0300

    Change title

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -108,7 +108,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="titles">
-                            <h2>SurfsideMedia <span>Choice</span> of Services</h2>
+                            <h2>Home Services <span>Choice</span> of Services</h2>
                             <i class="fa fa-plane"></i>
                             <hr class="tall">
                         </div>
